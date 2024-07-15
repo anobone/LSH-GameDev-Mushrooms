3a0e79a66722c50ed3c96d83b71c3ebba4392498
anobone <91073823+anobone@users.noreply.github.com>
2024-07-15 15:43:40 +0300

    MVP

--- Assets/Scripts/Conditions.cs
+++ Assets/Scripts/Conditions.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Conditions : MonoBehaviour
 {
     List<GameObject> enemies = new List<GameObject>();
+    [SerializeField] Sprite youWin;
+    [SerializeField] Sprite youLose;
+    [SerializeField] Health playerHealth;
+    [SerializeField] GameObject conditions;
     void Start()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy").ToList();
@@ -18,6 +23,15 @@ public class Conditions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemies == null)
+        {
+            conditions.SetActive(true);
+            conditions.GetComponent<Image>().sprite = youWin;
+        }
+        if (playerHealth.GetRemainingHealth <= 0)
+        {
+            conditions.SetActive(true);
+            conditions.GetComponent<Image>().sprite = youLose;
+        }
     }
 }
